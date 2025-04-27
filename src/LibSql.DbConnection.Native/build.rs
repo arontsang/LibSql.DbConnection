1593dc9768e4dddb4a705aa387f1507f3f6b6bb1
Aron Tsang <arontsang@gmail.com>
2025-04-27 22:13:13 +0800

    Add simple async test

--- src/LibSql.DbConnection.Native/build.rs
+++ src/LibSql.DbConnection.Native/build.rs
@@ -3,7 +3,9 @@ fn main()  {
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("Turso")
+        .csharp_use_function_pointer(true) 
         .csharp_namespace("LibSql.DbConnection.Bindings")
+        .csharp_class_accessibility("public")
         .generate_csharp_file("./bindings/NativeMethods.g.cs")
         .unwrap();
 }
--- src/LibSql.DbConnection.Native/src/lib.rs
+++ src/LibSql.DbConnection.Native/src/lib.rs
@@ -1,4 +1,10 @@
+use async_io::Timer;
 use libsql::Builder;
+use std::time::Duration;
+use futures_lite::future;
+use async_executor::StaticExecutor;
+
+static EXECUTOR: StaticExecutor = StaticExecutor::new();
 
 #[no_mangle]
 pub extern "C" fn my_add(x: i32, y: i32) -> i32 {
@@ -12,6 +18,9 @@ pub struct Context {
     pub bar: i32,
     baz: u64
 }
+
+
+
 // 
 // #[no_mangle]
 // pub extern "C" fn create_list(
@@ -23,8 +32,23 @@ pub struct Context {
 //     list = Box::into_raw(ret);
 //     ()
 // }
+#[no_mangle]
+pub extern "C" fn tick() {
+    // println!("Tick!");
+    // future::block_on(EXECUTOR.tick());
+}
 
+#[no_mangle]
+pub extern "C" fn sleep(trampoline: extern "C" fn (usize) -> (), callback: usize) {
+    let task = smol::spawn(async move{
+        println!("Begin sleeping!");
+        Timer::after(Duration::from_millis(1)).await;
+        println!("Finished sleeping!");
+        trampoline(callback);        
+    });
 
+    task.detach();
+}
 
 #[no_mangle]
 pub extern "C" fn create_context() -> *mut Context {
